57c9b242a1678a1376aaa7f4c8587822081d97df
makiintoch <makiintoch@gmail.com>
2019-11-05 12:50:49 +0200

    adding shoping cart data

---
+++ lib/providers/cart.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    @required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {...items};
+  }
+
+  void addItem(String id, String title, double price) {
+    if (_items.containsKey(id)) {
+      _items.update(
+          id,
+          (existingCartItem) => CartItem(
+                id: existingCartItem.id,
+                title: existingCartItem.title,
+                price: existingCartItem.price,
+                quantity: existingCartItem.quantity + 1,
+              ));
+    } else {
+      _items.putIfAbsent(
+        id,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
+    }
+  }
+}
